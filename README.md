e2324e746c1b22b85f1fc8569ac38e2110ee3724
Roman Shamin <al4emist.artway@gmail.com>
2017-07-08 18:03:44 +0300

    Init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Trying to setup Sketch updating system
+
+Steps
+
+1. Add entry `appcast` to manifest.json with an URL to the appcast file.
+2. Add a appcast.xml
+3. Implement the Startup and Shutdown methods
---
+++ Updating Plugins.sketchplugin/Contents/Sketch/Updating Plugins.js
@@ -0,0 +1,22 @@
+var pluginVersion;
+
+var onShutdown = function(context) {
+  var action = context.actionContext;
+  var document = action.document;
+
+  document.hideMessage();
+  document.showMessage('Shutdown');
+}
+
+var onStartup = function(context) {
+  pluginVersion = '1.0';
+}
+
+var onRun = function(context) {
+  var document = context.document;
+  var pluginName = 'Updating Plugins';
+
+  document.showMessage(pluginName + ' ' + pluginVersion + ' is saying: Hello!');
+}
+
+onRun(context)
